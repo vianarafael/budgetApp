f99cb2877d48d9a10a15b9f86a362996f9879e35
vianarafael <vianarafaelds@gmail.com>
2019-12-28 15:00:25 +0900

    Created a Module Pattern - the backbones of the app

---
+++ app.js
@@ -0,0 +1,15 @@
+// Data Controller
+const budgetController = (function() {
+
+
+})()
+// UI Controller
+const UIController = (function() {
+
+})()
+// App controller
+const controller = (function(budgetCtrl, UICtrl) {
+
+
+
+})(budgetController, UIController) // if the names of budget and UI controllers are changed that will not interfere with the controller

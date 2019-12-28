6fc46183025d72c7a728da9ee411d602f800ebf7
vianarafael <vianarafaelds@gmail.com>
2019-12-28 15:20:56 +0900

    Setting up Event Listener for add button and key press

--- app.js
+++ app.js
@@ -3,12 +3,39 @@ const budgetController = (function() {
 
 
 })()
+
 // UI Controller
 const UIController = (function() {
 
 })()
-// App controller
+
+// Global App Controller
 const controller = (function(budgetCtrl, UICtrl) {
+
+    const ctrlAddItem = function() {
+    // 1. Get the input data
+
+    // 2. add the item to the budget controller
+
+    // 3. add the item to the UI
+
+    // 4. calculate the budget
+
+    // 5 display the result of the calculation
+    console.log('?')
+    }
+
+    // When the add button is clicked ..
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    // .. or when the enter key is pressed
+    document.addEventListener('keypress', function(e) {
+        // older browsers use which instead of keyCode
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem
+        }
+    })
+
 
 
 
